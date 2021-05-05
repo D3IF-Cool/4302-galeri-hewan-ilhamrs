3291dcb4f01e42ae738939830bc72a86915a45cf
ilham_ramadhani_saputro <dkijakartaramasaputro@gmail.com>
2021-05-05 12:32:06 +0700

    Refactor fitur main ke fragment

--- app/src/main/java/org/d3if3051/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if3051/galerihewan/model/Hewan.kt
@@ -1,4 +1,4 @@
-package org.d3if3051.galerihewan
+package org.d3if3051.galerihewan.model
 
 data class Hewan (
     val nama: String, val namaLatin: String, val imageResId: Int
--- app/src/main/java/org/d3if3051/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if3051/galerihewan/ui/main/MainAdapter.kt
@@ -1,8 +1,9 @@
-package org.d3if3051.galerihewan
+package org.d3if3051.galerihewan.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if3051.galerihewan.model.Hewan
 import org.d3if3051.galerihewan.databinding.ListItemBinding
 
 class MainAdapter(private val data: List<Hewan>):
